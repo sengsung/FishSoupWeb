6351457c338729f5d54c5eb76e0a4acbbbe8cd98
FishSoup_KOR <whdqhr345@gmail.com>
2018-03-29 21:16:42 +0900

    Add files via upload

---
+++ pin/assets/js/main.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $(".typed").typed({
+        strings: ["생각이 없어요.", "생각하고 싶지 않아요.", "생각하기 귀찮아요.", "숨쉬기도 귀찮아요.", "잠자기도 귀찮아요.", "생선스프입니다."],
+        stringsElement: null,
+        typeSpeed: 120,
+        startDelay: 0,
+        backSpeed: 80,
+        backDelay: 800,
+        loop: true,
+        showCursor: false
+    });
+});
